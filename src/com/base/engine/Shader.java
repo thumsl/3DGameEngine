49836271069864ab53fd78609b8be04927cf9add
Luan Cristian Thums <luanc@outlook.com>
2015-04-24 19:51:19 -0300

    Episode 11

--- src/com/base/engine/Shader.java
+++ src/com/base/engine/Shader.java
@@ -3,11 +3,15 @@ package com.base.engine;
 import static org.lwjgl.opengl.GL20.*;
 import static org.lwjgl.opengl.GL32.*;
 
+import java.util.HashMap;
+
 public class Shader {
 	private int program;
+	private HashMap<String, Integer> uniforms;
 	
 	public Shader() {
 		program = glCreateProgram();
+		uniforms = new HashMap<String, Integer>();
 		
 		if (program == 0) {
 			System.err.println("Shader creation failed: Could not find valid memory location in constructor");
@@ -17,6 +21,18 @@ public class Shader {
 	
 	public void bind() {
 		glUseProgram(program);
+	}
+	
+	public void addUniform(String uniform) {
+		int uniformLocation = glGetUniformLocation(program, uniform);
+		
+		if (uniformLocation == -1) {
+			System.err.println("Error: could not find uniform " + uniform + ".");
+			new Exception().printStackTrace();
+			System.exit(1);
+		}
+		
+		uniforms.put(uniform, uniformLocation);
 	}
 	
 	public void addVertexShader(String text) {
@@ -64,5 +80,21 @@ public void compileShader() {
 		}
 		
 		glAttachShader(program, shader);
+	}
+
+	public void setUniformi(String uniformName, int value) {
+		glUniform1i(uniforms.get(uniformName), value);
+	}
+	
+	public void setUniformf(String uniformName, float value) {
+		glUniform1f(uniforms.get(uniformName), value);
+	}
+	
+	public void setUniform(String uniformName, Vector3f value) {
+		glUniform3f(uniforms.get(uniformName), value.getX(), value.getY(), value.getZ());
+	}
+
+	public void setUniform(String uniformName, Matrix4f value) {
+		glUniformMatrix4(uniforms.get(uniformName), true, Util.createFlippedBuffer(value));
 	}
 }
